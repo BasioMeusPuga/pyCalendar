68976f2b38632ca3e34fcb04eb2eee74e3df699e
BasioMeusPuga <disgruntled.mob@gmail.com>
2017-02-10 16:46:38 +0530

    Slight cleanup

--- pyCalendar.py
+++ pyCalendar.py
@@ -30,8 +30,8 @@ class colors:
 def calendar_show(timeframe):
 	events_to_show = []
 
-	# Display all events that occur today as csv
-	# This is mostly for displaying in a conky instance or something similar
+	""" Display all events that occur today as csv
+	This is mostly for displaying in a conky instance or something similar """
 	if timeframe == 'BlankForAllIntensivePurposes':
 
 		# Do not show events in case they're marked seen in the status table
@@ -53,8 +53,8 @@ def calendar_show(timeframe):
 		else:
 			return ', '.join(events_to_show)
 
-	# Display all events in the provided timeframe
-	# This shows as a table not really suited for parsing elsewhere
+		""" Display all events in the provided timeframe
+		This shows as a table not really suited for parsing elsewhere """
 	else:
 		if timeframe.isdigit() is True:
 			tf_pf = '+'
@@ -159,13 +159,13 @@ def calendar_add():
 	except:
 		print(colors.RED + 'Invalid date.' + colors.ENDC)
 		exit()
-	event_repeat = input('Repeat? (YES/no) ')
-	if event_repeat != 'no':
+	event_repeat = input('Repeat? (Y/n) ')
+	if event_repeat.lower() != 'no' or event_repeat.lower() != 'n':
 		event_repeat = 'yes'
 
-	event_insert = input('Insert into Database? (yes/NO) ')
-	if event_insert == 'yes':
-		database.execute("INSERT INTO events (Name,Date,Repeat) VALUES ('{0}','{1}','{2}')".format(event_name, event_date, event_repeat))
+	event_insert = input('Insert into Database? (y/N) ')
+	if event_insert.lower() == 'yes' or event_insert.lower() == 'y':
+		database.execute("INSERT INTO events (Name,Date,Repeat) VALUES ('{0}','{1}','yes')".format(event_name, event_date))
 		database.commit()
 
 
@@ -185,8 +185,8 @@ def calendar_seen():
 
 
 def parse_ics(ics_file):
-	# The idea is to parse the .ics file for birthdays (and anniversaries)
-	# That means checking for 1 day long yearly recurrent events
+	""" The idea is to parse the .ics file for birthdays (and anniversaries)
+	That means checking for 1 day long yearly recurrent events """
 
 	# Open calendar file.
 	my_cal = open(realpath(ics_file), 'r')
@@ -212,7 +212,7 @@ def parse_ics(ics_file):
 
 		time_delta = date_object_end - date_object_start
 		if time_delta.days > 1:
-			return False, None
+			return (False,)
 
 		# Reject if UNTIL date has already passed
 		if 'UNTIL=' in event[2]:
@@ -220,27 +220,26 @@ def parse_ics(ics_file):
 			date_object_until = datetime.datetime.strptime(until_date, '%Y%m%d')
 			today = datetime.datetime.today()
 			if today - date_object_until < datetime.timedelta(0):  # This is the 'official' way to calculate a negative timedelta
-				return False, None
+				return (False,)
 
-		# If the above conditions aren't met, return True
-		# With a dictionary object containing the name and the date
-		# This will continue to be repeated
+		""" If the above conditions aren't met, return True
+		With a dictionary object containing the name and the date
+		This will continue to be repeated """
 
 		birthday_event = {
 			'Name': event[3][8:],
 			'Date': date_object_start
 		}
-		return True, birthday_event
+		return (True, birthday_event)
 
 	# Run through the .ics file
 	event_list = []
 	_copy = False
 	for i in _calendar:
 
-		# Calendar events start with BEGIN:VEVENT, and end with END:VEVENT.
-		# We want them as long as RRULE:FREQ=YEARLY is present
-		# and optionally, the UNTIL value hasn't been exceeded by the
-		# time the next event occurs
+		""" Calendar events start with BEGIN:VEVENT, and end with END:VEVENT.
+		We want them as long as RRULE:FREQ=YEARLY is present
+		and the UNTIL value hasn't been exceeded """
 
 		if _copy is False:
 			if i == 'BEGIN:VEVENT\n':
@@ -258,9 +257,9 @@ def parse_ics(ics_file):
 			if positives and positives[0] not in invalid_entries:
 				this_event.append(positives[0].replace('\n', ''))
 
-	# We have the parsed data at this point
-	# Check to see if you really, really want to remember these ungrateful people
-	# and then add the relevant events to the calendar
+	""" We have the parsed data at this point
+	Check to see if you really, really want to remember these ungrateful people
+	and then add the relevant events to the calendar """
 	_confirm = input('Found {0} events. Add them to the database? (y/N) '.format(len(event_list)))
 	if _confirm == 'y' or _confirm == 'Y':
 		for j in event_list:
